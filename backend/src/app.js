2967fbe194f070dbddcf9b4bdb0a61dfc27ddb3d
Shreyas <92122065+shreydevs@users.noreply.github.com>
2025-05-29 01:00:27 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,15 @@ const cors=require('cors');
 const app=express();
 
 app.use(cors())
+
+app.use(cors(
+  {
+    origin: [https://code-criticai.vercel.app/"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
