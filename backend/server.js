967425e49b6b48d700448551c8f38a55e883b40c
Shreyas <92122065+shreydevs@users.noreply.github.com>
2025-05-29 10:13:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const app=require('./src/app');
-require('dotenv').config();
+  const app = require('./src/app');
+  require('dotenv').config();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
