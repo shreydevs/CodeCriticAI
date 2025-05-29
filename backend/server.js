af7dfff8886ab3126fbe9bd7403ec1997e5c989c
Shreyas <92122065+shreydevs@users.noreply.github.com>
2025-05-29 11:21:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
-  const app = require('./src/app');
-  require('dotenv').config();
-
 const express = require('express');
-const app = express();
+
 
 // Add this middleware to enable CORS
-app.use((req, res, next) => {
+express.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://code-criticai.vercel.app'); // Or '*' to allow any origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Allowed methods
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
   next();
 });
+
+
+const app = require('./src/app');
+  require('dotenv').config();
+
+
 
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
