78cbc53ed3b7075af9285fe1940e305e02d385ac
Shreyas <92122065+shreydevs@users.noreply.github.com>
2025-05-29 10:59:20 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,12 @@ const aiRoutes=require('./routes/ai.routes');
 const cors=require('cors');
 const app=express();
 
-app.use(cors(
-  {
-    origin: ["https://code-criticai.vercel.app"],
-    methods: ["POST, "GET"],
-    credentials:true
-  }
-))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://code-criticai.vercel.app'); // Or '*' to allow any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Allowed methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+  next();
+});
 
 
 
