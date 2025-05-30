fda45209cc6720c2fb62e0de1ab7b782f4f39ac0
Shreyas <92122065+shreydevs@users.noreply.github.com>
2025-05-31 00:32:57 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors=require('cors');
 const app=express();
 
 app.use(cors({
-  origin: 'https://code-criticai.vercel.app',
+  origin: 'https://code-critic-ai.vercel.app',
   methods: ['GET', 'POST', 'OPTIONS'],
   credentials: true
 }));
